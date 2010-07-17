422df092a8fd0595ffd88eefdeebf73fa0ba1123
LucasTizma <lucastizma@lucastizma.com>
2010-07-17 15:27:39 -0400

    Added license information to every header file

--- Samples/FastTableViewCells/Classes/FastTableViewCellsAppDelegate.m
+++ Samples/FastTableViewCells/Classes/FastTableViewCellsAppDelegate.m
@@ -1,9 +1,21 @@
 //
-//  FastTableViewCellsAppDelegate.m
-//  FastTableViewCells
+//  Copyright 2010 Michael Shannon Potter
 //
-//  Created by Michael Potter on 5/10/2010.
-//  Copyright LucasTizma 2010. All rights reserved.
+//  LucasTizma
+//  www.lucastizma.com
+//  lucastizma@lucastizma.com (E-mail/AIM)
+//
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
 //
 
 #import "FastTableViewCellsAppDelegate.h"
@@ -20,7 +32,8 @@
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
 	self.window = [[[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds] autorelease];
-	self.navigationController = [[[UINavigationController alloc] initWithRootViewController:[[[RootViewController alloc] initWithStyle:UITableViewStylePlain] autorelease]] autorelease];
+	self.navigationController = [[[UINavigationController alloc] initWithRootViewController:[[[RootViewController alloc]
+        initWithStyle:UITableViewStylePlain] autorelease]] autorelease];
     [self.window addSubview:self.navigationController.view];
     [self.window makeKeyAndVisible];
 
